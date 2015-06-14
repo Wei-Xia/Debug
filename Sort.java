9ff7bc35535c6ef8368eb8e05a0f8a3657b68a3b
Wei-Xia <xw900812@gmail.com>
2015-06-14 17:23:47 -0400

    1

---
+++ Sort.java
@@ -0,0 +1,39 @@
+//课时51  排序
+
+import java.util.*;
+
+public class Sort{
+
+	public static void sort (int[] a){
+
+		for (int i =0; i<a.length;i++){
+			int idx = 0;
+		for (int j=1; j<a.length-i;){
+			if( a[j]> a[idx]) {
+				idx = j;
+			}
+		}
+
+		int t = a[idx];
+		a[idx] = a[a.length-i-1];
+		a[a.length-i-1]=t;
+	}
+}
+
+public static void main (String[] args) {
+
+	int[] a = new int[6];
+	for (int i=0; i<a.length; i++)
+		a[i]=(int)(Math.random()*100);
+	for (int k:a){
+		System.out.print(k+ " ");
+	}
+	System.out.println();
+	sort(a);
+	for (int k:a) {
+		System.out.print(k+ " ");
+	}
+	System.out.print(k+ " ");
+}
+
+}
