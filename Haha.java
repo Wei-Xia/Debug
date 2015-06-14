d58bb34f2f5b1b4291ee630749f0fdceecff57a3
Wei-Xia <xw900812@gmail.com>
2015-06-14 17:23:51 -0400

    2

---
+++ Haha.java
@@ -0,0 +1,16 @@
+
+
+public class Haha{
+
+public static void UpperCase(String s){	
+	String s = "hello";
+	g(s);
+	String t=s;
+	t = "bye";
+	int i = t.length();
+	System.out.println (s.indexOf("e"));
+}
+
+
+}
+
