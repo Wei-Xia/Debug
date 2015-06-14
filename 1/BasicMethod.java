ed8ac425146862c3c98a96ca2a270b1c775a2d54
Wei-Xia <xw900812@gmail.com>
2015-06-14 17:22:38 -0400

    4

---
+++ 1/BasicMethod.java
@@ -0,0 +1,23 @@
+public class BasicMethod {
+	
+	public static int max(int a , int b) {
+		return a>b?a:b;		
+	}
+
+	public static void sayMorining() {
+		System.out.println("Morning!");
+	}
+	
+	public class BasicMethodTest {
+	
+	public static void main(String[] args) {
+
+		x = max(5,8);
+		x = max(x,10);
+
+		sayMorining();
+
+	}
+}
+
+}
